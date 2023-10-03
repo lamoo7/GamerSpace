5abc78a15df5c5401abaecdd747c6098ad15fc5b
Gorcea Radu Andrei <80151170+lamoo7@users.noreply.github.com>
2023-10-03 19:03:10 +0300

    Delete GamerSpace directory

--- GamerSpace/profile/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("click", function(event) {
-    if (event.target === document.getElementById("dialogElement")) {
-        document.getElementById("dialogElement").close();
-        document.getElementById("dialogElement").innerHTML = '<div id="mini-loader"></div>';
-    }
-});
