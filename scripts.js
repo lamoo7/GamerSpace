4cc1800cffc75b7813414e1494eea3c9933adc25
Gorcea Radu Andrei <80151170+lamoo7@users.noreply.github.com>
2023-10-03 19:04:03 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("click", function(event) {
+  	if (event.target === document.getElementById("dialogElement")) {
+     	document.getElementById("dialogElement").close();
+		document.getElementById("dialogElement").innerHTML = '<div id="mini-loader"></div>';
+  	}
+});
